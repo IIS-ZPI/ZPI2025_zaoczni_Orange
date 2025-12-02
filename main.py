eb386015a22ac1acf167dc81fbf61f7b32f7c463
Łysy <mikolaj.orzechowski@interia.pl>
2025-12-02 19:05:14 +0100

    sub operation

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ print("Orange, Developer Scrum Master, AleksanderPiasecki")
 print("Orange, Developer, lysy246351")
 print("Orange, Tester, FernoNowsky")
 print("Orange, DevOps, Paweł Pater")
+
+
+def arithmetic_sub(a: float, b: float):
+    return a - b
